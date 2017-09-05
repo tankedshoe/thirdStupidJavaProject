8b55e77e6fabd9a4e86162f1775d175c9af84f07
tankedshoe <tankedshoe@gmail.com>
2017-09-05 09:26:26 -0600

    Initial commit

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,9 @@
+package stupid.controller;
+
+public class StupidController
+{
+public void start()
+{
+	System.out.println("My name is Dane and I love slacklining.");
+}
+}
---
+++ src/stupid/controller/StupidRunnerDeux.java
@@ -0,0 +1,10 @@
+package stupid.controller;
+
+public class StupidRunnerDeux
+{
+public static void main(String [] args)
+{
+	StupidController firstApp = new StupidController();
+	firstApp.start();
+}
+}
